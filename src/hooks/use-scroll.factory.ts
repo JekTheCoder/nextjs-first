3eaa5c244fd2f5858aa233687512886be2c28a98
Jeshua <hinostrozajeshua@gmail.com>
2023-02-12 11:02:16 -0500

    feat: limit pokemon fetching

--- src/hooks/use-scroll.factory.ts
+++ src/hooks/use-scroll.factory.ts
@@ -1,31 +1,33 @@
-import { useState, useRef, useCallback, useEffect } from 'react'
-import { useInfiniteQuery, useQuery } from 'react-query'
+import { useRef, useCallback } from 'react'
+import { useInfiniteQuery } from 'react-query'
 
 type GetterFn<T> = (limit: number, offset: number) => Promise<T[]>
 
 export function scrollHookFactory<T>(querykey: string, getterFn: GetterFn<T>) {
-  return (limit: number) => {
-    const limitRef = useRef(limit)
+	return (limit: number) => {
+		const limitRef = useRef(limit)
 
-    const setLimit = useCallback(
-      (limit: number) => (limitRef.current = limit),
-      []
-    )
+		const setLimit = useCallback(
+			(limit: number) => (limitRef.current = limit),
+			[]
+		)
 
-    const { fetchNextPage, data, isLoading, isError, isFetching } = useInfiniteQuery(
-      querykey,
-      {
-        queryFn: ({ pageParam }) => getterFn(limitRef.current, pageParam),
-        getNextPageParam: (_, pages) =>
-          pages.reduce((acc, next) => acc + next.length, 0),
-      }
-    )
+		const { fetchNextPage, data, isLoading, isError, isFetching } = useInfiniteQuery(
+			querykey,
+			{
+				queryFn: ({ pageParam }) => getterFn(limitRef.current, pageParam),
+				getNextPageParam: (_, pages) => {
+					const len = pages.reduce((acc, next) => acc + next.length, 0)
+					return len > 1000 ? undefined : len
+				},
+			}
+		)
 
-    return [
-      data?.pages.flat(),
-      fetchNextPage,
-      { isLoading, isError, isFetching },
-      setLimit,
-    ] as const
-  }
+		return [
+			data?.pages.flat(),
+			fetchNextPage,
+			{ isLoading, isError, isFetching },
+			setLimit,
+		] as const
+	}
 }
